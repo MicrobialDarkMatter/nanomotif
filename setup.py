e86b9ce55bcd3e443e10f728d84805fe1573b9b9
SorenHeidelbach <shei@bio.aau.dk>
2023-09-21 13:32:14 +0200

    Added dependencies

--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
 from setuptools import setup, find_packages
 
-setup(name='nanomotif',
-      version='0.1',
-      description='Identifying methlyation motifs in nanopore data',
-      author='AAU_DarkScience',
-      author_email='shei@bio.aau.com',
-      license='MIT',
-      packages=find_packages(),
-      zip_safe=False)
+setup(
+    name='nanomotif',
+    version='0.0.3',
+    description='Identifying methlyation motifs in nanopore data',
+    author='AAU_DarkScience',
+    author_email='shei@bio.aau.com',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        "wheel",
+        "requests",
+        "numpy==1.24.3",
+        "pandas==2.0.2",
+        "polars==0.18.3",
+        "seaborn==0.12.2",
+        "scipy==1.10.1",
+
+    ],
+    entry_points={
+            'console_scripts': [
+                  'nanomotif = nanomotif.main:main'
+            ]
+    }
+)
