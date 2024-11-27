ab6500e55924affd6c83c626ae2a38355ab629c2
SebastianDall <semoda@bio.aau.dk>
2024-11-27 13:21:49 +0100

    changed to hdbscan

--- nanomotif/binnary/detect_contamination.py
+++ nanomotif/binnary/detect_contamination.py
@@ -1,59 +1,85 @@
-from sklearn.manifold import TSNE
+import hdbscan
 import polars as pl
 from nanomotif.binnary import data_processing as dp
 import logging
 
-def detect_contamination(contig_methylation, contig_lengths, args):
+def detect_contamination(contig_methylation, contig_lengths):
     logger = logging.getLogger(__name__)
     logger.info("Starting contamination detection analysis...")
 
-    contig_names, matrix = dp.create_matrix(contig_methylation)
-
-    logger.info("Starting tSNE")
-    tsne = TSNE(n_components = 3, perplexity = 30, random_state=42, n_jobs=args.threads)
-    tsne_embedding = tsne.fit_transform(matrix)
-
-    contig_weight = contig_lengths\
-        .filter(pl.col("contig").is_in(contig_names))\
-        .with_columns(
-            pl.col("length").log(base=10).alias("weight")
-        )\
-        .get_column("weight")
-
-    from sklearn.cluster import DBSCAN
-    logger.info("Performing DBSCAN")
-    dbscan = DBSCAN(eps=0.5, min_samples=5, n_jobs=args.threads)
-    dbscan.fit(tsne_embedding, sample_weight = contig_weight)
-    labels = dbscan.labels_
-
-    results = pl.DataFrame({
-                               "contig": contig_names,
-                               "cluster": labels
-                           })
-
-    results = contig_methylation\
+    # Create the bin_consensus dataframe for scoring
+    log.info("Creating bin_consensus dataframe for scoring...")
+    contig_methylation = data_processing.impute_contig_methylation_within_bin(
+        contig_methylation
+    )
+    # INFO: It does not makes sense to find contamination in bins with a single contig
+    single_contig_bins = contig_methylation\
         .select(["bin", "contig"])\
         .unique()\
-        .join(results, on = "contig", how = "left")
+        .group_by("bin")\
+        .agg(pl.col("contig").count().alias("n_contigs"))\
+        .filter(pl.col("n_contigs") == 1)\
+        .get_column("bin")
 
-    logger.info("Finding contamination in bins")
-    
-    # Filter contig_bin == bin and contig_bin_comparison_score > 0
-    contamination_contigs = contig_bin_comparison_score \
-        .filter(
-            (pl.col("bin") == pl.col("contig_bin")) &
-            (pl.col("binary_methylation_mismatch_score") > 0)
+    contig_methylation = contig_methylation\
+        .filter(~pl.col("bin").is_in(single_contig_bins))
+
+    contig_names, matrix = dp.create_matrix(contig_methylation)
+
+    logger.info("Performing HDBSCAN")
+    clusterer = hdbscan.HDBSCAN(min_samples=10, min_cluster_size=2, metric = "euclidean", allow_single_cluster=False)
+    labels = clusterer.fit_predict(matrix)
+
+    contig_bin = contig_methylation\
+        .select(["bin", "contig"])\
+        .unique() 
+
+    results = pl.DataFrame({
+                   "contig": contig_names,
+                   "cluster": labels
+              })\
+                .join(contig_bin, on="contig")\
+
+    bin_size = contig_bin\
+        .join(contig_lengths, on = "contig")\
+        .group_by("bin")\
+        .agg(
+            pl.col("length").sum().alias("bin_length"),
+            pl.col("contig").count().alias("n_contigs_bin")
         )
-    
-    contamination_contigs = contamination_contigs \
-        .drop("contig_bin") \
-            .rename(
-                {
-                    "bin_compare": "bin_contig_compare"
-                }
-            ) \
-        .sort("bin", "bin_contig_compare")
 
+    cluster_sizes = results\
+        .join(contig_lengths, on="contig")\
+        .group_by(["bin","cluster"])\
+        .agg(
+            pl.col("contig").count().alias("n_contigs"),
+            pl.col("length").sum().alias("cluster_length")
+        )\
+        .join(bin_size, on = "bin")\
+        .with_columns(
+            (pl.col("n_contigs") / pl.col("n_contigs_bin")).alias("fraction_contigs"),
+            (pl.col("cluster_length") / pl.col("bin_length")).alias("fraction_length")
+        )
+
+    assigned_cluster = cluster_sizes\
+        .group_by(["bin"])\
+        .agg(
+            pl.col("cluster_length").max().alias("cluster_length")
+        )\
+        .join(cluster_sizes, on = ["bin", "cluster_length"], how = "left")\
+        .rename({
+                    "cluster": "bin_cluster"
+                })\
+        .drop(["cluster_length", "n_contigs"])
+
+    results = results\
+        .join(assigned_cluster, on = ["bin"])
+
+    
+    logger.info("Finding contamination in bins")
+
+    contamination_contigs = results\
+        .filter(pl.col("bin_cluster") != pl.col("cluster"))
     
     logger.info("Contamination detection complete")
     
