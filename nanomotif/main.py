d7bdc16114f80b259e6403c9f358f5cc6282a948
SorenHeidelbach <shei@bio.aau.dk>
2025-04-11 09:35:18 +0200

    Reordering of postprocessing steps for find motifs bin

--- nanomotif/main.py
+++ nanomotif/main.py
@@ -472,15 +472,6 @@ def find_motifs_bin(args, pl,  pileup = None, assembly = None, min_mods_pr_conti
     motifs_file_name = motifs_file_name + "-score"
     save_motif_df(motifs, motifs_file_name)
 
-    log.info(" - Removing sub motifs")
-    motifs = motifs.rename({"bin":"contig"})
-    motifs = nm.postprocess.remove_sub_motifs(motifs)
-    if len(motifs) == 0:
-        log.info("No motifs found")
-        return
-    motifs_file_name = motifs_file_name +   "-sub"
-    motifs = motifs.rename({"contig":"bin"})
-    save_motif_df(motifs, motifs_file_name)
 
     log.info(" - Removing noisy motifs")
     motifs = nm.postprocess.remove_noisy_motifs(motifs)
@@ -498,6 +489,16 @@ def find_motifs_bin(args, pl,  pileup = None, assembly = None, min_mods_pr_conti
     motifs_file_name = motifs_file_name +   "-merge"
     save_motif_df(motifs, motifs_file_name)
 
+    log.info(" - Removing sub motifs")
+    motifs = motifs.rename({"bin":"contig"})
+    motifs = nm.postprocess.remove_sub_motifs(motifs)
+    if len(motifs) == 0:
+        log.info("No motifs found")
+        return
+    motifs_file_name = motifs_file_name +   "-sub"
+    motifs = motifs.rename({"contig":"bin"})
+    save_motif_df(motifs, motifs_file_name)
+    
     log.info(" - Joining motif complements")
     motifs = motifs.rename({"bin":"contig"})
     motifs = nm.postprocess.join_motif_complements(motifs)
