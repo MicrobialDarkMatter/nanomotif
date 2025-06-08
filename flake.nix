2ec1b99255e6f043ad6472860521d8b16f93b3d9
sebdal <semoda@bio.aau.dk>
2025-06-08 09:24:09 +0000

    added flake.nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A flake for the trustmap repo";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            # For python package
+            pkgs.python3Full
+            pkgs.maturin
+          ];
+        };
+    });
+}
